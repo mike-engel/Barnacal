0bfdc86fb32a5b227f89d7138c92c6b75cb05ad1
Mike Engel <mike@mike-engel.com>
2017-07-29 16:34:54 -0600

    ReasonML/Electron starter

---
+++ src/Index.re
@@ -0,0 +1,1 @@
+ReactDOMRe.renderToElementWithId <h1> (ReasonReact.stringToElement "Hello World!") </h1> "app"
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: ["./lib/js/src/index.js"],
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "./public"),
+    publicPath: "/public"
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
